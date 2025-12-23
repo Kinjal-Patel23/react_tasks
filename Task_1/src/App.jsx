b63f6a5693cf0642157f8ce6a953fb6231cc1ab8
Kinjal-Patel23 <kkaneriya11@gmail.com>
2025-12-23 11:15:33 +0530

    task_1

---
+++ Task_1/src/App.jsx
@@ -0,0 +1,14 @@
+import Portfolio from './component/Portfolio'
+
+const App = () => {
+  
+ const bio = "My Name is Kinjal Kaneriya. I have completed my BCA and I have also completed Full stack development classes at Red and White Institute"
+  
+  return (
+    <>
+      <Portfolio  bio = {bio}/>
+    </>
+  )
+}
+
+export default App
