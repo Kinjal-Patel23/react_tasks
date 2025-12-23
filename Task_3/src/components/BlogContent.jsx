6cc467cc07a96eab925498255f01bbd760b02441
Kinjal-Patel23 <kkaneriya11@gmail.com>
2025-12-23 13:03:08 +0530

    Task_3

---
+++ Task_3/src/components/BlogContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BlogContent = ({ title, content, onDelete }) => {
+    return (
+        <>
+            <div className='w-[25%] h-50 p-5 border border-gray-200 rounded-lg shadow-sm'>
+                <h2 className="text-xl font-semibold text-gray-800 mb-3">{title}</h2>
+                <p className="text-gray-600 text-sm leading-relaxed">{content}</p>
+                <button onClick={onDelete} className='w-20 h-10 border border-gray-300 mt-10'>
+                    Delete
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default BlogContent
