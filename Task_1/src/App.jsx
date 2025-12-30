747cfd64eb1da5f3488b03d2cc3b03d051b9e0a7
kinjal-kaneriya <kinjalk.encircle@gmail.com>
2025-12-30 12:04:06 +0530

    Task-1 done

--- Task_1/src/App.jsx
+++ Task_1/src/App.jsx
@@ -1,14 +1,15 @@
-import Portfolio from './component/Portfolio'
+import { Fragment } from "react";
+import Portfolio from "./component/Portfolio";
 
 const App = () => {
-  
- const bio = "My Name is Kinjal Kaneriya. I have completed my BCA and I have also completed Full stack development classes at Red and White Institute"
-  
+  const bio =
+    "My Name is Kinjal Kaneriya. I have completed my BCA and I have also completed Full stack development classes at Red and White Institute";
+
   return (
     <>
-      <Portfolio  bio = {bio}/>
+      <Portfolio bio={bio} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Task_1/src/component/Portfolio.jsx
+++ Task_1/src/component/Portfolio.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import profile from '../assets/profile_img.jpeg'
+import React from "react";
+import profile from "../assets/profile_img.jpeg";
 
-const Portfolio = ({bio}) => {
-
+const Portfolio = ({ bio }) => {
   const aboutMe = {
-    photo : profile,
-    name : "Kinjal Kaneriya",
-}
+    photo: profile,
+    name: "Kinjal Kaneriya",
+  };
 
   return (
     <>
-    <div style={{textAlign:'center', margin:'20px'}}>
-      <h1>My Portfolio</h1>
-      <img src={aboutMe.photo} alt="img" style={{width:"200px", height: "200px"}}/>
-      <h1>{aboutMe.name}</h1>      
-      <p>{bio}</p>
+      <div style={{ textAlign: "center", margin: "20px" }}>
+        <h1>My Portfolio</h1>
+        <img
+          src={aboutMe.photo}
+          alt="img"
+          style={{ width: "200px", height: "200px" }}
+        />
+        <h1>{aboutMe.name}</h1>
+        <p>{bio}</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
