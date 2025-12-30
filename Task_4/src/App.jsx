c8eafa17678ccf30983745ae54c0c99955d94c0c
kinjal-kaneriya <kinjalk.encircle@gmail.com>
2025-12-30 12:30:39 +0530

    Complete Task 4

--- Task_4/src/App.jsx
+++ Task_4/src/App.jsx
@@ -1,15 +1,13 @@
-import React from "react";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const App = () => {
-
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
     gender: "",
-    skills: []
+    skills: [],
   });
 
   const [submitData, setSubmitData] = useState(null);
@@ -35,17 +33,16 @@ const App = () => {
       return;
     }
 
-    setSubmitData(form)
-
+    setSubmitData(form);
 
     setForm({
       name: "",
       email: "",
       password: "",
       gender: "",
-      skills: []
+      skills: [],
     });
-  }
+  };
 
   const handleInput = (e) => {
     const { name, value, checked } = e.target;
@@ -53,26 +50,25 @@ const App = () => {
     if (name === "skills") {
       if (checked) {
         setForm({ ...form, skills: [...form.skills, value] });
+      } else {
+        setForm({
+          ...form,
+          skills: form.skills.filter((skill) => skill !== value),
+        });
       }
-      else {
-        setForm({ ...form, skills: form.skills.filter((skill) => skill !== value) });
-      }
-    }
-    else {
+    } else {
       setForm({ ...form, [name]: value });
     }
-  }
+  };
 
   return (
     <div className="min-h-screen flex flex-col gap-2 items-center justify-center bg-linear-to-r from-pink-300 via-purple-500 to-blue-300">
       <div className="bg-white w-full max-w-md p-8 rounded-2xl shadow-2xl">
-
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">
           Registration Form
         </h1>
 
         <form className="space-y-4" onSubmit={handleForm}>
-
           <input
             type="text"
             name="name"
@@ -113,12 +109,24 @@ const App = () => {
             <p className="text-gray-700 font-semibold mb-2">Gender</p>
             <div className="flex gap-6">
               <label className="flex items-center gap-2">
-                <input type="radio" name="gender" value="Male" className="accent-purple-500" onChange={handleInput} />
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Male"
+                  className="accent-purple-500"
+                  onChange={handleInput}
+                />
                 Male
               </label>
 
               <label className="flex items-center gap-2">
-                <input type="radio" name="gender" value="Female" className="accent-purple-500" onChange={handleInput} />
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Female"
+                  className="accent-purple-500"
+                  onChange={handleInput}
+                />
                 Female
               </label>
             </div>
@@ -128,7 +136,12 @@ const App = () => {
             <p className="text-gray-700 font-semibold mb-2">Skills</p>
             {["React js", "Node js", "Express js", "MongoDB"].map((skill) => (
               <label key={skill} className="block">
-                <input type="checkbox" name="skills" value={skill} onChange={handleInput} />{" "}
+                <input
+                  type="checkbox"
+                  name="skills"
+                  value={skill}
+                  onChange={handleInput}
+                />{" "}
                 {skill}
               </label>
             ))}
@@ -140,20 +153,26 @@ const App = () => {
           >
             Register
           </button>
-
         </form>
       </div>
       {submitData && (
         <div className="ml-8 bg-white p-6 rounded-xl shadow-lg">
           <h2 className="text-xl font-bold mb-2">Submitted Data</h2>
-          <p><strong>Name:</strong> {submitData.name}</p>
-          <p><strong>Email:</strong> {submitData.email}</p>
-          <p><strong>Gender:</strong> {submitData.gender}</p>
-          <p><strong>Skills:</strong> {submitData.skills.join(", ")}</p>
+          <p>
+            <strong>Name:</strong> {submitData.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {submitData.email}
+          </p>
+          <p>
+            <strong>Gender:</strong> {submitData.gender}
+          </p>
+          <p>
+            <strong>Skills:</strong> {submitData.skills.join(", ")}
+          </p>
         </div>
       )}
     </div>
-
   );
 };
 
