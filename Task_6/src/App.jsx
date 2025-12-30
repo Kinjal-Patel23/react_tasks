5b964eb231805e3e4542cd81cbec3add65b2f820
kinjal-kaneriya <kinjalk.encircle@gmail.com>
2025-12-30 12:43:27 +0530

    Complete Task 6

--- Task_6/src/App.jsx
+++ Task_6/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import BlogList from './components/BlogList'
-import BlogPage from './pages/BlogPage'
-import NotFound from './pages/NotFound'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BlogList from "./components/BlogList";
+import BlogPage from "./pages/BlogPage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<BlogList/>}/>
-          <Route path='/blog/:id' element={<BlogPage/>}/>
-          <Route path='*' element={<NotFound/>}/>
+          <Route path="/" element={<BlogList />} />
+          <Route path="/blog/:id" element={<BlogPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Task_6/src/components/BlogList.jsx
+++ Task_6/src/components/BlogList.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import BlogContent from './BlogContent';
-import { Link } from 'react-router-dom';
-import blogs from '../data/blogs';
+import { useState } from "react";
+import BlogContent from "./BlogContent";
+import { Link } from "react-router-dom";
+import blogs from "../data/blogs";
 
 const BlogList = () => {
-
   const [post, setPost] = useState(blogs);
 
   const handleDeleteBlog = (id) => {
     const blogDelete = post.filter((blog) => blog.id !== id);
     setPost(blogDelete);
-  }
+  };
 
   return (
     <>
-      <div className='w-full mt-12 px-10 flex flex-wrap justify-center gap-8'>
+      <div className="w-full mt-12 px-10 flex flex-wrap justify-center gap-8">
         {post.map((p) => (
           <BlogContent
             key={p.id}
-            title={
-              <Link to={`/blog/${p.id}`}>{p.title}</Link>
-            }
+            title={<Link to={`/blog/${p.id}`}>{p.title}</Link>}
             content={p.content}
-            onDelete={() => { handleDeleteBlog(p.id) }}
+            onDelete={() => {
+              handleDeleteBlog(p.id);
+            }}
           />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default BlogList
+export default BlogList;
