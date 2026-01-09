77e951e79396a341be0f6c577a65bfedad1930b6
kinjal-kaneriya <kinjalk.encircle@gmail.com>
2026-01-09 15:49:05 +0530

    feat: add todo app using useState

--- task_2/src/App.jsx
+++ task_2/src/App.jsx
@@ -1,48 +1,12 @@
-import { useState } from 'react'
+import { Todo } from './component/Todo'
 
 const App = () => {
 
-  const [input, setInput] = useState('');
-  const [task, setTask] = useState([]);
-
-
-  const handleInput = (e) => {
-    setInput(e.target.value);
-  }
-
-  const handleAddButton = (e) => {
-    e.preventDefault();
-
-    if(input.trim() === "") return
-
-    setTask([...task, input]);
-    setInput("");
-  }
-
-  const handleDeleteBtn = (id) => {
-    const deleteTask = task.filter((_, i) => i !== id)
-    setTask(deleteTask);
-  }
-
+  
   return (
-    <>
-      <h1>TODO LIST</h1>
-      <form onSubmit={handleAddButton}>
-        <input type="text" required placeholder='Enter your task here...' value={input} onChange={handleInput} />
-        <button>Add</button>
-      </form>
-
-      {task.length > 0 ? (
-        <ul>
-          {task.map((t, i) => (
-            <li key={i}>{t} <button onClick={() => handleDeleteBtn(i)}>Delete</button></li>
-          ))}
-        </ul>
-      ) : (
-        <p>No Task added yet</p>
-      )}
-
-    </>
+    <div>
+      <Todo/>
+    </div>
   )
 }
 
